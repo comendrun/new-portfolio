b40546d922d2d4a0f6f8e0f2be1244145a19bc83
comendrun <kamran.ruhani@gmail.com>
2022-07-16 22:57:57 +0200

    add two old projects + change background for hover effect of portfolio items

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -35,7 +35,7 @@ export default function Projects() {
                 <i
                   title={each.name}
                   key={nanoid()}
-                  className={each.display}
+                  className={`${each.display} icons`}
                 ></i>
               );
             })}
