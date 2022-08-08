1da4dea92621be871c3af10704b0bd28f4c8ff3f
KamRan <kamran.rouhani.dev@gmail.com>
2022-08-08 14:50:31 +0200

    deleted all the files to start over

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import { useState } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import HomePage from "./Pages/HomePage";
-import ProjectPage from "./Pages/Projects/ProjectPage";
-
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-  return (
-    <div className={`app ${darkMode && "dark"}`}>
-      <BrowserRouter>
-        <Header darkModeOnClick={() => setDarkMode((preValue) => !preValue)} />
-
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-
-          <Route path="/projects">
-            <ProjectPage />
-          </Route>
-        </Switch>
-
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
