86e66ff923026fa33617806c73e5b7ce6ac5e9d3
KamRan <kamran.rouhani.dev@gmail.com>
2022-08-21 23:02:13 +0200

    edited appwrapper for social media

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BsTwitter, BsInstagram, BsFacebook } from "react-icons/bs";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <BsLinkedin />
       </div>
       <div>
         <FaFacebookF />
       </div>
       <div>
-        <BsInstagram />
+        <BsGithub />
       </div>
     </div>
   );
